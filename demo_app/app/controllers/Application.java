044f7a91f6f67b9ad6f0be06472744c95e18128a
suzui <yechenfan@vip.163.com>
2018-06-11 15:52:58 +0800

    m

--- demo_app/app/controllers/Application.java
+++ demo_app/app/controllers/Application.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import enums.ProStatus;
+import models.back.Pro;
 import play.db.jpa.NoTransaction;
+
+import java.io.File;
+import java.util.List;
 
 public class Application extends BaseController {
     
@@ -10,6 +15,15 @@ public class Application extends BaseController {
     }
     
     public static void test() {
+        List<Pro> pros = Pro.fetchAll();
+        pros.stream().filter(p -> p.location.contains("app")).forEach(p -> {
+            if ( p.check().read.contains("java")) {
+                p.status(ProStatus.NORMAL);
+            } else {
+                p.status(ProStatus.STOP);
+            }
+        });
+        
         renderJSON("test");
     }
 }
