e637d70e1386f74b6d2c3d94f2b0273b20edc9f3
suzui <yechenfan@vip.163.com>
2018-06-11 11:59:13 +0800

    m

--- demo_app/app/controllers/Application.java
+++ demo_app/app/controllers/Application.java
@@ -1,15 +1,11 @@
 package controllers;
 
-import play.Logger;
-import play.Play;
 import play.db.jpa.NoTransaction;
 
 public class Application extends BaseController {
     
     @NoTransaction
     public static void index() {
-        Logger.info(Play.applicationPath.getAbsolutePath());
-        Logger.info(Play.getFile(Play.applicationPath.getAbsolutePath()).getAbsolutePath());
         renderHtml("start...");
     }
     
