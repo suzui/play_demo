b27bc4791352772ca88c180e657fa344a1d35ec9
suzui <yechenfan@vip.163.com>
2018-06-13 15:27:51 +0800

    m

--- demo_job/app/jobs/Test0Job.java
+++ demo_job/app/jobs/Test0Job.java
@@ -1,6 +1,5 @@
 package jobs;
 
-import javafx.concurrent.Task;
 import play.jobs.OnApplicationStart;
 
 @OnApplicationStart
@@ -9,17 +8,8 @@ public class Test0Job extends BaseJob {
     @Override
     public void doJob() throws Exception {
         super.doJob();
-        //System.err.println("test0...");
+        System.err.println("test0...");
     }
     
-    public static void main(String[] args) {
-        Test0Job job = new Test0Job();
-        Task t = new Task() {
-            
-            @Override
-            protected Object call() throws Exception {
-                return null;
-            }
-        };
-    }
+    
 }
