8b221449253592a94d46572d231f23a3e9b3bea0
suzui <yechenfan@vip.163.com>
2018-09-14 13:50:58 +0800

    m

--- demo_app/app/controllers/Application.java
+++ demo_app/app/controllers/Application.java
@@ -1,25 +1,25 @@
 package controllers;
 
 import annotations.ActionMethod;
-import models.person.Person;
+import play.Play;
 import play.db.jpa.NoTransaction;
 import utils.BaseUtils;
 import vos.PersonVO;
-
-import java.util.List;
 
 public class Application extends BaseController {
     
     @NoTransaction
     public static void index() {
+        System.err.println(Play.id);
         renderHtml("start...");
     }
     
     @ActionMethod(name = "测试", clazz = PersonVO.class)
     public static void test(PersonVO vo) {
-        
-        List<Person> persons = Person.fetchAll();
-        System.err.println(BaseUtils.modelToId(persons));
+        String a = BaseUtils.property("a");
+        String b = BaseUtils.property("b");
+        System.err.println(a);
+        System.err.println(b);
         renderJSON("test");
     }
     
