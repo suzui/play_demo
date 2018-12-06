46da88ed88a4df2c7c67da2522dbcdfab9888cef
suzui <yechenfan@vip.163.com>
2018-12-06 11:19:25 +0800

    m

--- demo_app/app/controllers/Application.java
+++ demo_app/app/controllers/Application.java
@@ -1,14 +1,8 @@
 package controllers;
 
 import annotations.ActionMethod;
-import models.organize.Organize;
-import models.person.Person;
-import models.token.BasePerson;
-import net.sf.ehcache.hibernate.EhCacheProvider;
 import play.db.jpa.NoTransaction;
 import vos.PersonVO;
-
-import java.util.List;
 
 public class Application extends BaseController {
     
@@ -17,18 +11,18 @@ public class Application extends BaseController {
         renderHtml("start...");
     }
     
-    @ActionMethod(name = "测试", clazz = PersonVO.class)
+    @ActionMethod(name = "测试", param = "name", clazz = PersonVO.class)
     public static void test(PersonVO vo) {
-        List<Organize> list = Organize.fetchAll();
-        BasePerson person = list.get(1).person();
-        System.err.println(person);
-        //System.err.println(person.area);
-        //Person person = Person.findByID(1l);
-        //System.err.println(person.area);
-        Organize organize = Organize.findByID(2l);
-        //Organize.findByID(2l);
-        //Person person = organize.person();
-        System.err.println(organize.person);
+        //List<Organize> list = Organize.fetchAll();
+        //BasePerson person = list.get(1).person();
+        //System.err.println(person);
+        ////System.err.println(person.area);
+        ////Person person = Person.findByID(1l);
+        ////System.err.println(person.area);
+        //Organize organize = Organize.findByID(2l);
+        ////Organize.findByID(2l);
+        ////Person person = organize.person();
+        //System.err.println(organize.person);
         renderJSON("test");
     }
     
