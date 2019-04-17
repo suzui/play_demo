c4105206c488c45ba99c99f7e0864cadbbab385d
suzui <yechenfan@vip.163.com>
2019-04-17 22:18:09 +0800

    m

--- demo_app_job/app/controllers/Application.java
+++ demo_app_job/app/controllers/Application.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import utils.BaseUtils;
+
 public class Application extends BaseController {
     
     public static void index() {
+        System.err.println(BaseUtils.property(""));
         renderHtml("job");
     }
     
