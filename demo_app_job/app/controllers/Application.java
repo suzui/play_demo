df6bd51f13c3628a70498e6ed94c6beb674fe298
suzui <yechenfan@vip.163.com>
2019-04-18 10:53:29 +0800

    m

--- demo_app_job/app/controllers/Application.java
+++ demo_app_job/app/controllers/Application.java
@@ -5,7 +5,7 @@ import utils.BaseUtils;
 public class Application extends BaseController {
     
     public static void index() {
-        System.err.println(BaseUtils.property(""));
+        System.err.println(BaseUtils.property("schedule"));
         renderHtml("job");
     }
     
